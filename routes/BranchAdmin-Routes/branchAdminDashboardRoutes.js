c73d14cb484f18bd0beea2b0b75c8af8a7009307
Devendra Selokar <selokardevendra05@gmail.com>
2025-04-24 15:23:34 +0530

    updated code

--- routes/BranchAdmin-Routes/branchAdminDashboardRoutes.js
+++ routes/BranchAdmin-Routes/branchAdminDashboardRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getTopSellingBrands,getProductCount,getAllOrdersDeliveredIncome,getAllDelieveryBoys,getAllOrders, getRecentTrackedOrders} = require("../../controllers/branchAdmin-Controllers/branchAdminDashboardController");
+const {getTopSellingBrands,
+    getProductCount,
+    getAllOrdersDeliveredIncome,
+    getAllDelieveryBoys,
+    getAllOrders
+} = require("../../controllers/branchAdmin-Controllers/branchAdminDashboardController");
 const branchAdminAuthMiddleware = require("../../middlewares/branchAdminMiddleware");
  
 
@@ -11,7 +16,7 @@ router.get("/totalOrderCount", getAllOrders);
 router.get("/delivered-income", getAllOrdersDeliveredIncome);
 router.get('/totalProductCount', getProductCount);
 router.get("/top-brands", getTopSellingBrands);
-router.get("/recent-orders",branchAdminAuthMiddleware,getRecentTrackedOrders);
+
 
  
 
